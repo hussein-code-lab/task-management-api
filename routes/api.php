d1bfeda0d60c5b77e8613cbc97f3da8d9677b0b7
Hussein <hussiencode78@gmail.com>
2026-07-31 18:42:13 +0300

    Implement Sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\V1\AuthController;
 use Illuminate\Support\Facades\Route;
+Route::prefix('v1/auth')->group(function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+    });
+
 });
